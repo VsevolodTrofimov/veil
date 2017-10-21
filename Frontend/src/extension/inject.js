d2e0c25a7de849e79c2f1b62eae0a57d08d31ec9
v-trof <seva.trofimov2@gmail.com>
2017-10-22 00:36:18 +0300

    Shipped game UI

--- Frontend/src/extension/inject.js
+++ Frontend/src/extension/inject.js
@@ -1,26 +1,44 @@
 // import game
 
-const $makeMessageBox = (text, from='bot') => {
-    const $message = document.createElement('div')
-    $message.innerText = text
-    $message.style.padding = '16px'
-    $message.style.marginBottom = '20px'
-
-    if(from === 'player') $message.style.alignSelf = 'flex-end'
-
-    return $message
+var gameMap = {
+    'clickbait': {
+        text: ['Достали тролли?'],
+        actions: [{
+            text: 'Ага!',
+            goto: 'resist'
+        }, {
+            text: 'Игнорю их и не парюсь',
+            goto: 'im good'
+        }]
+    },
+    'im good': {
+        text: ['Так держать! Но не забывай помогать тем, кто трепетнее к этому отностится'],
+        actions: []
+    }
 }
 
-const renderStage = ({$chatbox}, stage) => {
-    stage.text.forEach(text => {
-        $chatbox.appendChild($makeMessageBox(text))
-    })
+const $makeMessageBox = (text, from='bot') => {
+    const $message = document.createElement('div')
+    $message.textContent = text
+    $message.style.padding = '15px'
+    $message.style.marginBottom = '20px'
+    $message.style.border = '1px solid #eee'
+    
+    if(from === 'player') {
+        $message.style.alignSelf = 'flex-end'
+        $message.style.background = '#edf0f5'
+    } else {
+        $message.style.alignSelf = 'flex-start'
+    }
+
+    return $message
 }
 
 const $makeAction = (context, action) => {
     const $btn = document.createElement('button')
     $btn.classList.add('flat_button')
-    $btn.innerText = action.text
+    $btn.style.marginRight = '15px'
+    $btn.textContent = action.text
 
     $btn.addEventListener('click', () => transition(context, action))
 
@@ -34,9 +52,25 @@ const renderActions = (context, actions) => {
     });
 }
 
-const transition = (context, action) => {
+const renderStage = (context, stage) => {
+    context.map[stage].text.forEach(text => {
+        context.$chatbox.appendChild($makeMessageBox(text))
+    })
 
+    renderActions(context, context.map[stage].actions)
 }
+
+
+const transition = (context, action) => {
+    context.$chatbox.appendChild($makeMessageBox(action.text, 'player'))
+    renderStage(context, action.goto)
+}
+
+const makeContext = (map, $game) => ({
+    map,
+    $chatbox: $game.querySelector('.chat'),
+    $actionbox: $game.querySelector('.actions')
+})
 
 const $makeGame = () => {
     const $game = document.createElement('div')
@@ -48,7 +82,7 @@ const $makeGame = () => {
                         justify-content: flex-end;
                         border-radius: 4px;"
                       >
-                        <div class='chat' style="display:flex; felx-direction: column; justify-content: flex-start;"> </div>
+                        <div class='chat' style="display:flex; flex-direction: column; justify-content: flex-start; padding: 0 15px;"> </div>
                         <div class='actions submit_post' style="display:flex; felx-direction: row; flex-wrap: wrap;"> </div>
                       </div>`
 
@@ -75,7 +109,11 @@ const injectGame = () => {
 
     $post.querySelector('.post').appendChild($game)
 
-    const $postInserted = $wall.insertBefore($post, wall.children[8])
+    const context = makeContext(gameMap, $game)
+
+    renderStage(context, 'clickbait')
+
+    const $postInserted = $wall.insertBefore($post, $wall.children[8])
 }
 
 export default injectGame
