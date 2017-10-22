c3ffddeb478f9ad4ddb53d43130e7a706399476e
Ilia Sheshukov <sheshukov.ilya@gmail.com>
2017-10-22 09:50:25 +0300

    random bs

--- backend/vkhack.py
+++ backend/vkhack.py
@@ -132,7 +132,7 @@ def send_veils(userId):
 @app.route('/data/getDiscussions')
 def send_discussions():
     result = []
-    disc = Discussion.query.filter(Discussion.rated == False).limit(5).all()
+    disc = Discussion.query.filter(Discussion.rated == False).limit(2).all()
     for d in disc:
         dic = {}
         dic['postId'] = d.postId
