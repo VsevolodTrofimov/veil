b12486d52123bc7fbbde27062ed53e27447d125d
Ilia Sheshukov <sheshukov.ilya@gmail.com>
2017-10-22 05:07:13 +0300

    dbg

--- backend/vkhack.py
+++ backend/vkhack.py
@@ -31,65 +31,68 @@ with app.app_context():
     db.create_all()
     db.session.commit()
 
-    sio = socketio.Server()
+sio = socketio.Server()
 
-    clients = {}
-
-
-    @app.route('/')
-    def home():
-        return ''
+clients = {}
 
 
-    @sio.on('connect')
-    def connect(sid, environ):
-        print("CONNECTED: ", sid)
-        return 'OK'
+@app.route('/')
+def home():
+    return ''
 
 
-    @sio.on('disconnect')
-    def disconnect(sid):
-        print('DISCONNECTED: %s' % clients[sid])
+@sio.on('connect')
+def connect(sid, environ):
+    print("CONNECTED: ", sid)
+    return 'OK'
 
 
-    @sio.on('connected')
-    def get_userid_and_send_all_veils(sid, data):
-        print("HUY")
-        data = json.loads(data)
-        userId = data['userId'][1:]
-        response_raw = urllib.request.urlopen(
-            "https://api.vk.com/method/utils.resolveScreenName?screen_name=" + userId).read().decode("utf-8")
-        response = json.loads(response_raw)
-        userScreenName = str(response['response']['object_id'])
-
-        clients[str(sid)] = userScreenName
-        print(clients)
-
-        print("%s connected" % userScreenName)
-
-        send_veil()
+@sio.on('disconnect')
+def disconnect(sid):
+    print('DISCONNECTED: %s' % clients[sid])
 
 
-    def send_veil():
+@sio.on('connected')
+def get_userid_and_send_all_veils(sid, data):
+    print("HUY")
+    data = json.loads(data)
+    userId = data['userId'][1:]
+    response_raw = urllib.request.urlopen(
+        "https://api.vk.com/method/utils.resolveScreenName?screen_name=" + userId).read().decode("utf-8")
+    response = json.loads(response_raw)
+    userScreenName = str(response['response']['object_id'])
+
+    clients[str(sid)] = userScreenName
+    print(clients)
+
+    print("%s connected" % userScreenName)
+
+    send_veil()
+
+
+def send_veil():
+    with app.app_context():
         for d in Discussion.query.filter(Discussion.veiled == False).all():
             data = {}
             data['data'] = {}
             data['data']['post_id'] = d.postId
             data['data']['user_ids'] = [u for u in d.users if u != clients[request.sid]]
 
-            sio.emit('veil_send', data)
+        sio.emit('veil_send', data)
 
 
-    @sio.on('comment')
-    def receive_comment(sid, data):
-        our_guy = clients[str(sid)]
-        data = json.loads(data)
-        print(data)
+@sio.on('comment')
+def receive_comment(sid, data):
+    our_guy = clients[str(sid)]
+    data = json.loads(data)
+    print(data)
 
-        comment = Comment(data['commentId'], data['postId'], data['authorId'], data['text'], data['mentions'])
-        if comment.postId == "-1":
-            print("Discussion - bad id.")
-            sio.emit('comment_reply', 'Error. Bad id.')
+    comment = Comment(data['commentId'], data['postId'], data['authorId'], data['text'], data['mentions'])
+    if comment.postId == "-1":
+        print("Discussion - bad id.")
+        sio.emit('comment_reply', 'Error. Bad id.')
+
+    with app.app_context():
 
         response = Discussion.query.filter_by(postId=comment.postId).all()
         if (not response) or (comment.mentions == ['-1'] and comment.authorId == our_guy):
@@ -115,51 +118,54 @@ with app.app_context():
                 discussion.length = len(new_disc.comments)
             db.session.commit()
 
-        export_to_ml()
-        send_veil()
+    export_to_ml()
+    send_veil()
 
 
-    @sio.on('getveils')
-    def send_veils(userId):
-        pass
+@sio.on('getveils')
+def send_veils(userId):
+    pass
 
 
-    @app.route('/getDiscussions')
-    def send_discussions():
-        result = []
+@app.route('/getDiscussions')
+def send_discussions():
+    result = []
+    with app.app_context():
         disc = Discussion.query.filter(Discussion.rated == False).limit(5).all()
         for d in disc:
             dic = {}
             dic['postId'] = d.postId
             dic['comments'] = d.discussion
-            result.append(dic)
+        result.append(dic)
 
-        return (jsonify(result))
-
-
-    @app.route('/postDiscussion', methods=['POST'])
-    def receive_discussions_and_export_to_ml():
-        data = request.form['data']
-        dat = json.loads(data)
-
-        exp_path = path.abspath(path.join(getcwd(), "../ML/train_set"))
-
-        root_id = dat['comments'][0][0]
-        discussion_verdict = dat['offensive']
-
-        f = open(path.join(exp_path, "id_%s_%s_%s.txt" % (dat['postId'], root_id, discussion_verdict)), 'w')
-        s = ""
-        for c in dat['comments']:
-            com_id = c[0]
-            text = c[1]
-            verdict = c[2]
-            s += text + verdict + '\r\n' + com_id + "\r\n"
-            f.write(s)
-        f.close()
+    return (jsonify(result))
 
 
-    def export_to_ml():
-        exp_path = path.abspath(path.join(getcwd(), "../ML/pred_set"))
+@app.route('/postDiscussion', methods=['POST'])
+def receive_discussions_and_export_to_ml():
+    data = request.form['data']
+    dat = json.loads(data)
+
+    exp_path = path.abspath(path.join(getcwd(), "../ML/train_set"))
+
+    root_id = dat['comments'][0][0]
+    discussion_verdict = dat['offensive']
+
+    f = open(path.join(exp_path, "id_%s_%s_%s.txt" % (dat['postId'], root_id, discussion_verdict)), 'w')
+    s = ""
+    for c in dat['comments']:
+        com_id = c[0]
+        text = c[1]
+        verdict = c[2]
+        s += text + verdict + '\r\n' + com_id + "\r\n"
+        f.write(s)
+    f.close()
+
+
+def export_to_ml():
+    exp_path = path.abspath(path.join(getcwd(), "../ML/pred_set"))
+
+    with app.app_context():
 
         disc = Discussion.query.filter(Discussion.length >= 3).all()
         for d in disc:
@@ -175,35 +181,36 @@ with app.app_context():
             comments = [[k] + v for (k, v) in jsonpickle.decode(d.discussion)]
             new_disc = Base(d.rootId, comments)
             f.write(new_disc.print2csv_last(5))
-            f.close()
+        f.close()
 
 
-    def read_res_and_write_to_db():
-        k = []
-        res_path = path.abspath(path.join(getcwd(), "../ML/pred_res"))
-        for i in listdir(res_path):
-            a = open(path.join(res_path, i), 'r')
-            a = a.read()
-            k.append(tuple([re.split(r'[._]', i)[1:-2], a[0]]))
-            remove(path.join(res_path, i))
+def read_res_and_write_to_db():
+    k = []
+    res_path = path.abspath(path.join(getcwd(), "../ML/pred_res"))
+    for i in listdir(res_path):
+        a = open(path.join(res_path, i), 'r')
+        a = a.read()
+        k.append(tuple([re.split(r'[._]', i)[1:-2], a[0]]))
+        remove(path.join(res_path, i))
 
+    with app.app_context():
         for v in k:
             d = Discussion.query.filter(Discussion.postId == v[0][0]).filter(Discussion.rootId == v[0][1]).first()
             d.veiled = True if (v[1][1] == '1') else False
-            db.session.commit()
+        db.session.commit()
 
 
-    if __name__ == '__main__':
-        # socket.run(app, port=5000, keyfile='key.pem', certfile='cert.pem')
-        #   app.run(app, port=5000)
-        app = socketio.Middleware(sio, app)
+if __name__ == '__main__':
+    # socket.run(app, port=5000, keyfile='key.pem', certfile='cert.pem')
+    #   app.run(app, port=5000)
+    app = socketio.Middleware(sio, app)
 
-        eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('194.67.208.71', 5000)),
-                                               certfile='/etc/letsencrypt/live/vkhack.v-trof.ru/cert.pem',
-                                               keyfile='/etc/letsencrypt/live/vkhack.v-trof.ru/privkey.pem',
-                                               server_side=True,
-                                               ssl_version=ssl.PROTOCOL_SSLv23),
-                             app)
+    eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('194.67.208.71', 5000)),
+                                           certfile='/etc/letsencrypt/live/vkhack.v-trof.ru/cert.pem',
+                                           keyfile='/etc/letsencrypt/live/vkhack.v-trof.ru/privkey.pem',
+                                           server_side=True,
+                                           ssl_version=ssl.PROTOCOL_SSLv23),
+                         app)
 
 
-        # eventlet.wsgi.server(eventlet.listen(('194.67.208.71', 5000)), app)
+# eventlet.wsgi.server(eventlet.listen(('194.67.208.71', 5000)), app)
