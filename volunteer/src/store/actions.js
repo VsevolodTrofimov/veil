3bc4d3bef7f67bf2c9694c3b95712ccf07674dba
v-trof <seva.trofimov2@gmail.com>
2017-10-22 03:09:40 +0300

    honestly fetching from server

--- volunteer/src/store/actions.js
+++ volunteer/src/store/actions.js
@@ -26,9 +26,10 @@ const packDiscussions = discussions => {
 export const getDiscussions = ({commit}) => {
     //fetch
     fetch('/data/getDiscussions')
-        .then(res => res.text())
-        .then(console.log)
-    setTimeout(() =>
+        .then(res => res.json())
+        .then(data => commit('init', data))
+    
+    /* setTimeout(() =>
         commit('init', [{comments: [{
             authorId: '10',
             text: 'Passing Props to Route Components. Using $route in your component creates a tight coupling with the route which limits the flexibility of the component as it can ...'
@@ -39,7 +40,7 @@ export const getDiscussions = ({commit}) => {
             authorId: '5',
             text: 'Bad text'
         }]}, 
-    ]), 5000)
+    ]), 5000) */
 }
 
 export const sendToServer = ({state, commit, dispatch}) => {
